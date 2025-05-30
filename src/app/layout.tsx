5093fd3a0111a35dd6047b8e4f92f4ff65ac4759
Dhruv Suri <dhruvsuri789@gmail.com>
2025-05-30 10:30:23 +0530

    feat(setup): Setup complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import { repositoryName } from '@/prismicio';
+import { PrismicPreview } from '@prismicio/next';
+import localFont from 'next/font/local';
+
+import './globals.css';
+
+const gambarino = localFont({
+  src: '../../public/fonts/Gambarino-Regular.woff2',
+  weight: '100 900',
+  display: 'swap',
+  variable: '--font-gambarino',
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={gambarino.variable}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,21 +1,21 @@
+import { SliceZone } from '@prismicio/react';
 import {
   SliceSimulator,
   SliceSimulatorParams,
   getSlices,
-} from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+} from '@slicemachine/adapter-next/simulator';
 
-import { components } from "@/slices";
+import { components } from '@/slices';
 
-export default async function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
+export default async function SliceSimulatorPage({ searchParams }: SliceSimulatorParams) {
   const { state } = await searchParams;
   const slices = getSlices(state);
 
   return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
+    <SliceSimulator background="" zIndex={10}>
+      <div className="max-h-[900px]">
+        <SliceZone slices={slices} components={components} />
+      </div>
     </SliceSimulator>
   );
 }
