fb0e07b69afdcd1e109fac61b683cac7a8259fec
Dhruv Suri <dhruvsuri789@gmail.com>
2025-05-31 15:53:08 +0530

    feat(seo): Add fallback title, meta description and OG image to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import { repositoryName } from '@/prismicio';
+import { createClient, repositoryName } from '@/prismicio';
 import { PrismicPreview } from '@prismicio/next';
 import localFont from 'next/font/local';
 
 import { Footer } from '@/components/Footer';
 import NavBar from '@/components/Navbar';
+import { isFilled } from '@prismicio/client';
+import { Metadata } from 'next';
 import './globals.css';
 
 const gambarino = localFont({
@@ -11,6 +13,24 @@ const gambarino = localFont({
   display: 'swap',
   variable: '--font-gambarino',
 });
+
+// Fallback for every page
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle('settings');
+
+  return {
+    title: settings.data.site_title || 'Côte Royale Paris',
+    description:
+      settings.data.meta_description ||
+      'Discover the exquisite collection of luxury fragrances by Côte Royale Paris',
+    openGraph: {
+      images: isFilled.image(settings.data.fallback_og_image)
+        ? [settings.data.fallback_og_image.url]
+        : ['/cote-royale-og-image.png'],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
