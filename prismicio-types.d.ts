30c6a4dda32c31a9a8a1170a237fb335c4cafe10
Dhruv Suri <dhruvsuri789@gmail.com>
2025-05-31 13:05:48 +0530

    feat(section): Video section

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -126,6 +126,7 @@ export type FragranceDocument<Lang extends string = string> = prismic.PrismicDoc
 >;
 
 type PageDocumentDataSlicesSlice =
+  | VideoSlice
   | CallToActionSlice
   | FragranceListSlice
   | ProductFeatureSlice
@@ -569,6 +570,48 @@ type ScrollTextSliceVariation = ScrollTextSliceDefault;
  */
 export type ScrollTextSlice = prismic.SharedSlice<'scroll_text', ScrollTextSliceVariation>;
 
+/**
+ * Primary content in *Video → Default → Primary*
+ */
+export interface VideoSliceDefaultPrimary {
+  /**
+   * Youtube Video ID field in *Video → Default → Primary*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: eTEsWseiDdg
+   * - **API ID Path**: video.default.primary.youtube_video_id
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  youtube_video_id: prismic.KeyTextField;
+}
+
+/**
+ * Default variation for Video Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type VideoSliceDefault = prismic.SharedSliceVariation<
+  'default',
+  Simplify<VideoSliceDefaultPrimary>,
+  never
+>;
+
+/**
+ * Slice variation for *Video*
+ */
+type VideoSliceVariation = VideoSliceDefault;
+
+/**
+ * Video Shared Slice
+ *
+ * - **API ID**: `video`
+ * - **Description**: Video
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type VideoSlice = prismic.SharedSlice<'video', VideoSliceVariation>;
+
 declare module '@prismicio/client' {
   interface CreateClient {
     (
@@ -618,6 +661,10 @@ declare module '@prismicio/client' {
       ScrollTextSliceDefaultPrimary,
       ScrollTextSliceVariation,
       ScrollTextSliceDefault,
+      VideoSlice,
+      VideoSliceDefaultPrimary,
+      VideoSliceVariation,
+      VideoSliceDefault,
     };
   }
 }
